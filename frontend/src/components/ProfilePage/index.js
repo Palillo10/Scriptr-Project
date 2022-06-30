d6040dedfd58ac088999abaad547150af73d8fd4
Isaac <isaac.g.diaz10@gmail.com>
2022-06-29 22:50:39 -0500

    DRYed up my profile page component by moving all my modal code into a seperate component

--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -6,8 +6,13 @@ import { getAllUsers } from "../../store/users";
 import { createPicture, explorePictures } from "../../store/pictures";
 
 import UserSongs from "./UserSongs";
+import PictureModal from "./PictureModal";
+
 
 import './ProfilePage.css'
+
+
+
 const ProfilePage = () => {
   const dispatch = useDispatch()
   const { username } = useParams();
@@ -24,44 +29,6 @@ const ProfilePage = () => {
   }
 
 
-
-  const [name, setName] = useState('')
-  const [imageUrl, setImageUrl] = useState('')
-  const [modalBackground, setModalBackground] = useState(null);
-  // const [doneLoading, setDoneLoading] = useState(false)
-
-
-
-
-  const pictureSubmit = (e) => {
-    e.preventDefault()
-    const newPictureInfo = {
-      name,
-      imageUrl,
-      userId: user.id
-    }
-    dispatch(createPicture(newPictureInfo))
-
-    setName('')
-    setImageUrl('')
-    modalBackground.style.display = "none"
-  }
-
-  const handleClick = () => {
-    modalBackground.style.display = "flex"
-    // modalBackground.addEventListener("click", () => {
-    //   console.log("hello")
-    //   modalBackground.style.display = "none"
-    // })
-  }
-
-
-
-  React.useEffect(() => {
-    const ele = document.getElementsByClassName('modalBackground')[0]
-    setModalBackground(ele)
-  }, [user])
-
   React.useEffect(() => {
     dispatch(getAllUsers())
     dispatch(explorePictures())
@@ -69,50 +36,13 @@ const ProfilePage = () => {
 
 
 
-  // React.useEffect(() => {
-  //   let func = async () => {
-  //     console.log('func')
-  //     return await setDoneLoading(true)
-  //   }
-  //   func()
-  // }, [])
-  // if (!doneLoading) return ("Loading")
-  // if (doneLoading && !user) return ("Not Found")
-
-
 
   if (user) return (<div className="profilePageContainer">
-    <div className="modalBackground">
-      <div className="modal">
-        <button onClick={() => modalBackground.style.display = "none"}>X</button>
-        <fieldset>
-          <form onSubmit={pictureSubmit}>
-            <div>
-              <label htmlFor="name">Name </label>
-              <input type="text" id="name"
-                value={name}
-                onChange={e => setName(e.target.value)}
-              />
-            </div>
-            <div>
-              <label htmlFor="imageUrl">Picture URL </label>
-              <input type="text" id="imageUrl"
-                value={imageUrl}
-                onChange={e => setImageUrl(e.target.value)}
-              />
-            </div>
-            <button>Upload</button>
-          </form>
-        </fieldset>
-      </div>
-    </div>
     <div className="profileHeader">
       <img id="avatar" src={user.profilePic} alt="avatar" />
       <h1>{user.username}</h1>
     </div>
-    <div>
-      {currUser && currUser.username === user.username && <button onClick={() => handleClick()}>Upload Picture</button>}
-    </div>
+    <PictureModal user={user} currUser={currUser} />
     <UserSongs userPictures={userPictures} user={user} currUser={currUser} />
   </div>)
 
