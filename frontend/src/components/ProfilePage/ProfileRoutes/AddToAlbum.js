d730fd4b650b61e4a644bfc7527b96e8f0a64d90
Isaac <isaac.g.diaz10@gmail.com>
2022-07-04 01:25:16 -0500

    adjusted css for adding an album

--- frontend/src/components/ProfilePage/ProfileRoutes/AddToAlbum.js
+++ frontend/src/components/ProfilePage/ProfileRoutes/AddToAlbum.js
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux"
+import { useState } from "react"
 import "./AddToAlbumForm.css"
 
 
@@ -11,14 +12,20 @@ const AddToAlbum = ({ user }) => {
   if (user) {
     userPictures = pictures.filter(picture => picture.userId === user.id)
   }
-
+  const [examplePic, setExamplePic] = useState("https://cdnblog.picsart.com/2021/12/mid-post-3.jpg")
 
   return (<div className="AddToAlbumBackground">
     <div className="AddToAlbumModal">
-      <div className="Top"></div>
+      <div className="Top">
+        <button className="cancelButton" onClick={() => console.log("hello")}>X</button>
+        <div className="exampleImageContainer">
+          <img className="exampleImage" src={`${examplePic}`} />
+        </div>
+        <div className="TopBackground" style={{ backgroundImage: `url(${examplePic})` }}></div>
+      </div>
       <div className="choosePicture">
         {userPictures.map(picture => {
-          return (<img key={picture.id} className="pictureChoice" src={`${picture.imageUrl}`} />)
+          return (<img onClick={() => setExamplePic(picture.imageUrl)} key={picture.id} className="pictureChoice" src={`${picture.imageUrl}`} />)
         })}
       </div>
     </div>
